fdc0a00e81594d45f2af4ee0b06bce665312e49b
dikaysad <131869266+dikaysad@users.noreply.github.com>
2026-03-20 17:32:24 +0700

    update main.js
    duyệt

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
   header.style.background = window.scrollY > 50 ? "black" : "transparent";
 });
+
+const elements = document.querySelectorAll(".card, .review");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+elements.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(50px)";
+  observer.observe(el);
+});
